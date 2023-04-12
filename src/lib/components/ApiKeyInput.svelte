296cedd2a4f9e76921d20b899e57e062bc199db4
Daniel Vilela <dmvvilela@gmail.com>
2023-04-11 23:42:35 -0300

    obscure api key

--- src/lib/components/ApiKeyInput.svelte
+++ src/lib/components/ApiKeyInput.svelte
@@ -2,15 +2,15 @@
 	export let apiKey = '';
 </script>
 
-<div class="pt-2">
+<form class="pt-2">
 	<label for="chat" class="sr-only">Add your own OpenAI API Key</label>
 	<div class="flex items-center px-3 py-2 rounded-lg bg-gray-700">
-		<textarea
-			id="chat"
-			rows="1"
+		<input
+			id="api-key"
+			type="password"
 			class="block mx-2 p-2.5 w-full text-sm text-white outline-none rounded-lg border bg-gray-800 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
 			placeholder="Add your own OpenAI API Key"
 			bind:value={apiKey}
 		/>
 	</div>
-</div>
+</form>
